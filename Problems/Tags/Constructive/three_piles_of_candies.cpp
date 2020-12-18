7340a9b32a8fefd6df4fef8dafd217c33d96b438
joao-abrantes-uchoa <58981771+joaoabrantesuchoa@users.noreply.github.com>
2020-12-18 14:30:23 -0300

    Add files via upload

---
+++ Problems/Tags/Constructive/three_piles_of_candies.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const long long MOD = 1e9 + 7;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pii;
+typedef pair<int,pii> iii;
+typedef vector<pii> vii;
+typedef vector<vi> vvi;
+typedef priority_queue<pii, vector<pii>, greater<pii>> fila_prioridade;
+typedef priority_queue<iii, vector<iii>, greater<iii>> fila_prioridade_grids;
+typedef vector<vii> grafo_dj;
+
+
+#define preencher_negativo(memo) memset(memo, -1, sizeof memo) 
+#define preencher_zero(arr) memset(arr, 0, sizeof arr)
+#define sz(x) (int)x.size()
+#define nl '\n'
+#define INF 1000000000
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+#define NeedForSpeed ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+#define arredondar_para_cima(d) (int)((double)d + 0.5)
+#define all(a) a.begin(), a.end()
+#define EdgeList vector<pair<int,ii>>
+
+int q;
+ll arr[3];
+
+ll a,b,total, difference;
+
+int main() {
+        NeedForSpeed
+
+	cin >> q;
+
+	while(q--) {
+		cin >> arr[0] >> arr[1] >> arr[2];
+
+		sort(arr, arr + 3);
+
+
+		a = arr[0];
+		b = arr[1];
+
+		difference = b - a;
+
+		arr[2] -= difference; 
+
+		total = b + arr[2] / 2;
+
+		cout << total << nl;
+	}	
+
+        return 0;
+}
+
