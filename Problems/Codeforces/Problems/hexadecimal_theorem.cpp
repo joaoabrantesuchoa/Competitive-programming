cdb31a6986c9400d737360c7ff628e8747aa6e78
joao-abrantes-uchoa <58981771+joaoabrantesuchoa@users.noreply.github.com>
2021-01-08 21:35:40 -0300

    Add files via upload

---
+++ Problems/Codeforces/Problems/hexadecimal_theorem.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const long long MOD = 1e9 + 7;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pii;
+typedef pair<int,pii> iii;
+typedef vector<pii> vii;
+typedef vector<vi> vvi;
+typedef priority_queue<pii, vector<pii>, greater<pii>> fila_prioridade;
+typedef priority_queue<iii, vector<iii>, greater<iii>> fila_prioridade_grids;
+typedef vector<vii> grafo_dj;
+
+
+#define preencher_negativo(memo) memset(memo, -1, sizeof memo) 
+#define preencher_zero(arr) memset(arr, 0, sizeof arr)
+#define sz(x) (int)x.size()
+#define nl '\n'
+#define INF 1000000000
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+#define NeedForSpeed ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+#define arredondar_para_cima(d) (int)((double)d + 0.5)
+#define all(a) a.begin(), a.end()
+#define EdgeList vector<pair<int,ii>>
+
+int n;
+int fib[100009];
+	
+int main() {
+        NeedForSpeed
+
+	cin >> n;
+	
+	cout << n << " " << 0 << " " << 0 << nl;
+
+        return 0;
+}
+
