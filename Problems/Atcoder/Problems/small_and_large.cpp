5e9e6d8a62f294672e25e08d592cfd72a752b460
joao-abrantes-uchoa <58981771+joaoabrantesuchoa@users.noreply.github.com>
2021-01-18 10:27:07 -0300

    Add files via upload

---
+++ Problems/Atcoder/Problems/small_and_large.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const long long MOD = 1e9 + 7;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pii;
+typedef pair<int,pii> iii;
+typedef vector<pii> vii;
+typedef vector<vi> vvi;
+typedef priority_queue<pii, vector<pii>, greater<pii>> fila_prioridade;
+typedef priority_queue<iii, vector<iii>, greater<iii>> fila_prioridade_grids;
+typedef vector<vii> grafo_dj;
+
+
+#define preencher_negativo(memo) memset(memo, -1, sizeof memo) 
+#define preencher_zero(arr) memset(arr, 0, sizeof arr)
+#define sz(x) (int)x.size()
+#define nl '\n'
+#define INF 1000000000
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+#define NeedForSpeed ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+#define arredondar_para_cima(d) (int)((double)d + 0.5)
+#define all(a) a.begin(), a.end()
+#define EdgeList vector<pair<int,ii>>
+
+set<int> numeros;
+int a,b,k;
+
+int main() {
+        NeedForSpeed
+
+	cin >> a >> b >> k;
+
+	int numero = a;
+	int qtd = 0;
+
+	while(qtd < k && numero <= b && numero >= a) {
+		numeros.insert(numero);
+		qtd++;
+		numero++;
+	}
+
+	numero = b - k + 1;
+	qtd = 0;
+
+	while(qtd < k && numero <= b && numero >= a) {
+		if (numeros.find(numero) != numeros.end()) {
+			numero++;
+			continue;
+		} else {
+			numeros.insert(numero);
+			qtd++;
+			numero++;
+		}
+	}
+
+	for (auto x : numeros) cout << x << nl;
+
+	
+
+        return 0;
+}
+
---
+++ Problems/Atcoder/Problems/tax_increase.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const long long MOD = 1e9 + 7;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pii;
+typedef pair<int,pii> iii;
+typedef vector<pii> vii;
+typedef vector<vi> vvi;
+typedef priority_queue<pii, vector<pii>, greater<pii>> fila_prioridade;
+typedef priority_queue<iii, vector<iii>, greater<iii>> fila_prioridade_grids;
+typedef vector<vii> grafo_dj;
+
+
+#define preencher_negativo(memo) memset(memo, -1, sizeof memo) 
+#define preencher_zero(arr) memset(arr, 0, sizeof arr)
+#define sz(x) (int)x.size()
+#define nl '\n'
+#define INF 1000000000
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+#define NeedForSpeed ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+#define arredondar_para_cima(d) (int)((double)d + 0.5)
+#define all(a) a.begin(), a.end()
+#define EdgeList vector<pair<int,ii>>
+
+int a,b;
+
+int main() {
+        NeedForSpeed
+
+	cin >> a >> b;
+
+	bool encontrado = false;
+	int resposta;
+	float X1, X2;
+
+	for(int i = 1; i <= 1250; i++) {
+		for (int j = 1; j <= 1250; j++) {
+			X1 = floor(i * 0.08);
+			X2 = floor(j * 0.1);
+			
+			//cout << X1 << " " << X2 << nl;
+			if (X1 == a && X2 == b && i == j) {
+				encontrado = true;
+				resposta = i;
+				break;
+			}
+		}
+		if(encontrado)break;
+	}
+
+	if (encontrado) {
+		cout << resposta << nl;
+	} else {
+		cout << -1 << nl;
+	}
+
+        return 0;
+}
+
